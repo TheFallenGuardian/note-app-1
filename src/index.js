c0d5676e1ae6fdbc4dd4a8a61e7370e2a8ff26b9
TheFallenGuardian <71635876+TheFallenGuardian@users.noreply.github.com>
2025-04-12 10:35:31 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Optional, if you want to add some styles
+import App from './App'; // Ensure your App component is correctly named and located
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
